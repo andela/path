e1550e27c40a64d3d64e90c06884783b02c27caf
alex mwaleh <alex.mwaleh@andela.com>
2016-10-26 18:17:03 +0300

    [Feature #132647689] Add CSS Loaders
    - Added Css loaders
    - Added style.css

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,56 +1,45 @@
+/* eslint import/no-unresolved: "off"*/
+/* eslint import/no-extraneous-dependencies: "off"*/
 import React from 'react';
 import Form from 'muicss/lib/react/form';
 import Input from 'muicss/lib/react/input';
-import Textarea from 'muicss/lib/react/textarea';
 import Button from 'muicss/lib/react/button';
 import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 import Panel from 'muicss/lib/react/panel';
-const src = require('file!../images/MainLogo.png');
+import src from 'file!../images/MainLogo.png';
+import './style.css';
+
 
 const SignUp = () => {
-  const style = {
-    paddingRight: '1px'};
-
-  const child = { backgroundColor: '#038D6F',  padding:0,
-minHeight:'300px'
-};
-
-const vcenter = {
-color :'#fff',
-display:'block',
-marginLeft: 'auto',
-marginRight:'auto',
-height:'100px',
-
-}
   return (
 
-    <Row >
-      <Col lg="8"  sm="12" xs="12" style={child}>
+    <Row>
+      <Col lg="8" sm="12" xs="12" className="introDiv">
         <Col className="mui--pull-left" lg="4">
-          <div style={vcenter}>
-            <img src={src}/>
-             <h2 >register</h2>
+          <div className="vcenter">
+            <Col lg="12" className="mui--visible-xl-block mui--visible-lg-block">
+              <img className="mui--pull-right logo" src={src} alt="logo" />
+            </Col>
+            <Col>
+              <h1 className="mui--text-center">Register</h1>
+            </Col>
           </div>
-       </Col>
-      <Col lg="7" sm="12" xs="12"  className="mui--pull-right" style={child}>
-        <Panel  style={{marginBottom:'1px'}}>
-          <Form >
-
-            <Input hint="First Name" />
-            <Input hint="Last Name" />
-            <Input hint="Email" />
-            <Input hint="Password" />
-            <Input hint="Confirm Password" />
-            <Button variant="raised">Submit</Button>
-          </Form>
-        </Panel>
+        </Col>
+        <Col lg="7" sm="12" xs="12" className="mui--pull-right introDiv">
+          <Panel className="panel">
+            <Form>
+              <Input hint="First Name" />
+              <Input hint="Last Name" />
+              <Input hint="Email" />
+              <Input hint="Password" />
+              <Input hint="Confirm Password" />
+              <Button variant="raised">Submit</Button>
+            </Form>
+          </Panel>
+        </Col>
       </Col>
-</Col>
     </Row>
-
-
   );
 };
 
