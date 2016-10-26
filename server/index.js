a3efdca0bfba13ede22afa2453758a5f983a32fe
Humphrey Thuo <humphrey.thuo@andela.com>
2016-10-26 11:59:58 +0300

    Add hot reloading when running in dev environments

--- server/index.js
+++ server/index.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'development') {
+  /* eslint-disable import/no-extraneous-dependencies, global-require */
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackConfig = require('../webpack/webpack.config.dev');
+  /* eslint-enable import/no-extraneous-dependencies, global-require */
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -4,11 +4,15 @@ var webpack = require('webpack');
 var assetsPath = path.join(__dirname, '..', 'public', 'assets');
 
 module.exports = {
-  entry: path.join(__dirname, '../app/index.jsx'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '../app/index.jsx')
+  ],
   devtool: 'eval-source-map',
   output: {
     path: assetsPath,
     filename: 'bundle.js',
+    publicPath: '/assets/'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -19,5 +23,9 @@ module.exports = {
       loader: 'babel-loader',
       exclude: path.join(__dirname, '..', 'node_modules')
     }]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
