3b2f3470a54278d44a051b8bd8429abd7a85e2ac
Humphrey Thuo <humphrey.thuo@andela.com>
2016-11-18 12:17:05 +0300

    [Feature #133434213] Fetch user profile from API gateway
    For this to work the app must be deployed on an andela.com
    subdomain

--- app/components/Layout/Layout.jsx
+++ app/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import Overlay from 'material-ui/internal/Overlay';
 import cookie from 'react-cookie';
+import request from 'superagent';
 import NavBar from './NavBar';
 import SideBar from './SideBar';
 import testAvatar from '../../images/test_avatar.jpg';
@@ -11,7 +12,8 @@ class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebarCollapsed: true
+      sidebarCollapsed: true,
+      user: null,
     };
     this.toggleSidebar = this.toggleSidebar.bind(this);
     this.handleLogout = () => {
@@ -24,6 +26,17 @@ class Layout extends React.Component {
     if (!token) {
       browserHistory.replace('/login');
     }
+    request.get(`${API_GATEWAY_URL}/api/v1/users/me`)
+      .set('Authorization', `Bearer ${cookie.load('jwt-token')}`)
+      .then((res) => {
+        this.setState({
+          user: res.body,
+        });
+      })
+      .catch(() => {
+        cookie.remove('jwt-token');
+        browserHistory.push('/login');
+      });
   }
 
   toggleSidebar() {
@@ -38,11 +51,13 @@ class Layout extends React.Component {
       pointerEvents: !this.state.sidebarCollapsed ? 'auto' : 'none',
     };
 
+    const { user } = this.state;
+
     return (
       <div className="root">
         <NavBar
           toggleSidebar={this.toggleSidebar}
-          picture={testAvatar}
+          picture={(user && user.picture) || testAvatar}
           onLogout={this.handleLogout}
         />
         <SideBar
